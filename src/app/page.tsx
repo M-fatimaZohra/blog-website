ff2b15f2072df1655bbd780e68f8af718fc0032e
M-fatimaZohra <fatima.zohra10122007@gmail.com>
2025-01-01 21:09:44 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+//port Image from "next/image";
+import { Bebas_Neue } from "next/font/google";
+import { Merriweather } from "next/font/google";
+import { client } from "@/sanity/lib/client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import Link from "next/link"
+
+
+const merriweather = Merriweather({
+  weight: "400",
+  subsets: ['latin'],
+});
+
+const bebasNeue = Bebas_Neue({
+  weight: "400",
+  subsets: ['latin'],
+});
+interface Blog {
+  _id: string,
+  title: string,
+  description:string
+}
+async function getBlog() {
+
+  const Fetch = await client.fetch(`*[_type == 'blog']{ _id, title,description } `)
+
+
+  return Fetch
+}
+
+getBlog()
+export default async function Home() {
+
+  const blogData =await getBlog()
+
+
+
+  console.log(blogData)
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="py-20 flex flex-col justify-between items-center ">
+      <div className="flex justify-center items-center flex-col w-full">
+        <h1 className='lg:text-[125px] text-[80px] animate-[pulse_5s_ease-in-out_1] duration-500'style={bebasNeue.style}>Dewll Days</h1>
+        <p style={merriweather.style} className="lg:text-[18px] text-[16px]">Get to see blogs that match your taste</p>
+      </div>
+      <div className="grid grid-cols-1   xl:grid-cols-3  md:grid-cols-2 h-auto gap-4 py-10 px-8 sm:px-20 md:px-5 lg:px-5">
+        {blogData.slice(0,3).map((blog:Blog, index:number)=>{
+         return (
+          <div key={index} className="flex hover:animate-[pulse_500ms_ease-in-out_1]  justify-center flex-col translate-y-1 hover:-translate-y-3  ease-in duration-200 items-center gap-4 border-solid border-2 bg-[#dce3fa] text-gray-800 border-black rounded-lg w-auto  h-auto py-2  px-1 drop-shadow-[4px_2px_0px_rgba(0,0,0,0.25)]" >
+          <h1 className="lg:text-2xl text-[25px] font-bold">{blog.title}</h1>
+          <p className="lg:w-[400px]  text-[18px] line-clamp-[7] px-2  ">{blog.description}</p>
+          <Button variant="outline" size="sm" className="border-black drop-shadow-[2px_2px_0px_rgba(0,0,0,0.25)] active:translate-y-[2px] active:drop-shadow-none " ><Link href={`/Blog/${blog._id}`}>View</Link></Button>
+          </div>
+         )
+           
+        }) }
+       
+      </div>
+      <div>
+    <Link href="/Blog">
+     <Button variant="outline" size="default" className="border-black drop-shadow-[2px_2px_0px_rgba(0,0,0,0.25)] active:translate-y-[2px] active:drop-shadow-none ">View More...</Button>
+    </Link>
+    </div>
     </div>
   );
 }
